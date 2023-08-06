68b4651cd03cc4adc756324622fb38f4a9806130
Jens Skott <jens.skott@cathex.io>
2023-08-06 15:15:11 +0100

    README and Licence

--- pkg/terraform/generate.go
+++ pkg/terraform/generate.go
@@ -10,7 +10,7 @@ import (
 //go:embed templates/*
 var content embed.FS
 
-func (t *Terraform) Generate() error {
+func (t *Terraform) Generate(outFile string) error {
 
 	// Generate the main.tf template file
 	mainTemplate, err := content.ReadFile("templates/main.tf.tmpl")
@@ -42,6 +42,25 @@ func (t *Terraform) Generate() error {
 		if err := moduleTmpl.Execute(os.Stdout, module); err != nil {
 			return fmt.Errorf("error executing template: %w", err)
 		}
+	}
+
+	return nil
+}
+
+// TODO: ONe single function to generate and output the template
+func (t *Terraform) generateAndOutput(input, templateName, output string) error {
+	data, err := content.ReadFile(fmt.Sprintf("templates/%s.tf.tmpl", templateName))
+	if err != nil {
+		return fmt.Errorf("error reading template: %w", err)
+	}
+
+	tmpl, err := template.New(fmt.Sprintf("%s.tf", output)).Parse(string(data))
+	if err != nil {
+		return fmt.Errorf("error parsing template: %w", err)
+	}
+
+	if err := tmpl.Execute(os.Stdout, t); err != nil {
+		return fmt.Errorf("error executing template: %w", err)
 	}
 
 	return nil
